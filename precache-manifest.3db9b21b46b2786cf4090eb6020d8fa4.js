dd981ef7fa9470e8c0e5245d38eae3f939745feb
Varun Sharma <coding.with.binaries@gmail.com>
2019-10-23 00:35:52 +0530

    Updates

---
+++ precache-manifest.3db9b21b46b2786cf4090eb6020d8fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bf3a650589804eb9308878048c8513",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "bbd351ab182b93931655",
+    "url": "/profile/static/css/main.31dea251.chunk.css"
+  },
+  {
+    "revision": "ad7531ffc76018ab8310",
+    "url": "/profile/static/js/2.3c70dd3d.chunk.js"
+  },
+  {
+    "revision": "bbd351ab182b93931655",
+    "url": "/profile/static/js/main.1b5eff78.chunk.js"
+  },
+  {
+    "revision": "3fc111f4fff97b6dd47a",
+    "url": "/profile/static/js/runtime-main.201f941c.js"
+  }
+]);
